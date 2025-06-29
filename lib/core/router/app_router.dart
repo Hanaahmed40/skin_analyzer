c72a3c34783192e648f98cd6ea04abd21cd27fba
DESKTOP-8H0HA4B\aboghaly <ahmedghaly0767@gmail.com>
2025-06-29 13:37:56 +0300

    Handled initial route in AppRouter

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -4,6 +4,7 @@ import '../../forget_password/forget_screen.dart';
 import '../../auth/login/presentation/register_screen.dart';
 import '../../auth/login/presentation/login_screen.dart';
 import '../../ui/home_screen/home_screen.dart';
+import '../utils/app_utils.dart';
 import 'routes.dart';
 
 class AppRouter {
@@ -11,10 +12,12 @@ class AppRouter {
 
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
-      case '/':
-        return MaterialPageRoute(
-          builder: (_) => const LoginScreen(),
-        );
+      case '/': // initial route
+        return isUserLoggedIn ? _homeRoute() : _loginRoute();
+
+      case Routes.login:
+        return _loginRoute();
+
       case Routes.register:
         return MaterialPageRoute(builder: (_) => RegisterScreen());
 
@@ -22,11 +25,21 @@ class AppRouter {
         return MaterialPageRoute(builder: (_) => ForgetScreen());
 
       case Routes.home:
-        return MaterialPageRoute(builder: (_) => HomeScreen());
+        return _homeRoute();
 
       default:
         return _unFoundRoute();
     }
+  }
+
+  static MaterialPageRoute<dynamic> _homeRoute() {
+    return MaterialPageRoute(builder: (_) => HomeScreen());
+  }
+
+  static MaterialPageRoute<dynamic> _loginRoute() {
+    return MaterialPageRoute(
+      builder: (_) => const LoginScreen(),
+    );
   }
 
   static Route<dynamic> _unFoundRoute() {
