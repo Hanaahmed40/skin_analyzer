ed54e9b1af4635540ea845fda62ede97af4f7c1f
DESKTOP-8H0HA4B\aboghaly <ahmedghaly0767@gmail.com>
2025-06-29 16:28:34 +0300

    Refactored Analysis & Diagnosis Pages

--- lib/ui/home_screen/pages/analysisPage.dart
+++ lib/ui/home_screen/pages/analysisPage.dart
@@ -1,127 +1,69 @@
 import 'package:event_planning/utils/app_colors.dart';
+import 'package:event_planning/widget/picked_img_bloc_selector.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
-import 'diagnosisPage.dart';
-import 'homePage.dart';
-import 'dart:math';
 
-class AnalysisPage extends StatefulWidget {
-  @override
-  State<AnalysisPage> createState() => _AnalysisPageState();
-}
+import '../../../main_cubit.dart';
+import '../../../main_state.dart';
+import '../../../widget/image_picker_bloc_listener.dart';
 
-class _AnalysisPageState extends State<AnalysisPage> {
-  File? _image;
-
-  List<String> fakeResults = [
-    "Healthy skin detected",
-    "Mild irritation detected",
-    "Possible eczema detected",
-    "Suspicious skin condition detected",
-    "Dry skin identified"
-  ];
-
-  // Function to pick image from gallery
-  Future<void> _pickImageFromGallery() async {
-    final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
-    if (pickedFile != null) {
-      setState(() => _image = File(pickedFile.path));
-
-      // توليد نتيجة وهمية
-      String result = fakeResults[Random().nextInt(fakeResults.length)];
-
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => DiagnosisPage(
-            image: _image!,
-            result: result,
-            data: DateTime.now(),
-          ),
-        ),
-      );
-    }
-  }
-
-  // Function to capture image from camera
-  Future<void> _captureImageFromCamera() async {
-    final pickedFile = await ImagePicker().pickImage(source: ImageSource.camera);
-    if (pickedFile != null) {
-      setState(() => _image = File(pickedFile.path));
-
-      // توليد نتيجة وهمية
-      String result = fakeResults[Random().nextInt(fakeResults.length)];
-
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => DiagnosisPage(
-            image: _image!,
-            result: result,
-            data: DateTime.now(),
-          ),
-        ),
-      );
-    }
-  }
-
-  // عرض الصورة أو أيقونة بديلة
-  Widget imagePreview(File? image) {
-    return image == null
-        ? Icon(Icons.image_not_supported, size: 150, color: AppColors.greyColor)
-        : Image.file(image, height: 300, fit: BoxFit.cover);
-  }
+class AnalysisPage extends StatelessWidget {
+  const AnalysisPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         backgroundColor: AppColors.primaryLight,
-    ),
-    body: Center(
-    child: SingleChildScrollView(
-    child: Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-    imagePreview(_image),
-    SizedBox(height: 20),
-    _image == null
-    ? Column(
-    children: [
-    SizedBox(
-    width: 350,
-    height: 50,
-    child: ElevatedButton.icon(
-    onPressed: _pickImageFromGallery,
-    icon: Icon(Icons.photo_library_rounded, color: AppColors.whiteColor),
-    label: Text('Pick from Gallery', style: TextStyle(color: AppColors.whiteColor)),
-    style: ElevatedButton.styleFrom(
-    backgroundColor: AppColors.primaryLight,
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
-    ),
-    ),
-    ),
-    SizedBox(height: 15),
-    SizedBox(
-    width: 350,
-    height: 50,
-    child: ElevatedButton.icon(
-    onPressed: _captureImageFromCamera,
-    icon: Icon(Icons.camera_alt, color: AppColors.whiteColor),
-    label: Text('Take a Photo', style: TextStyle(color: AppColors.whiteColor)),
-    style: ElevatedButton.styleFrom(backgroundColor: AppColors.primaryLight,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
-    ),
-    ),
-    ),
-    ],
-    )
-        : Text("Image already selected!"),
-    ],
-    ),
-    ),
-    ),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            spacing: 20,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              BlocSelector<MainCubit, MainState, File?>(
+                selector: (state) => state.pickedImg,
+                builder: (context, pickedImg) {
+                  return pickedImg == null
+                      ? Icon(Icons.image_not_supported,
+                          size: 150, color: AppColors.greyColor)
+                      : PickedImgBlocSelector();
+                },
+              ),
+              const SizedBox(height: 15),
+              const ImagePickerBlocListener(),
+              ElevatedButton.icon(
+                onPressed: () =>
+                    context.read<MainCubit>().pickImg(ImageSource.gallery),
+                icon: Icon(Icons.photo_library_rounded,
+                    color: AppColors.whiteColor),
+                label: Text('Pick from Gallery',
+                    style: TextStyle(color: AppColors.whiteColor)),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: AppColors.primaryLight,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25)),
+                ),
+              ),
+              ElevatedButton.icon(
+                onPressed: () =>
+                    context.read<MainCubit>().pickImg(ImageSource.camera),
+                icon: Icon(Icons.camera_alt, color: AppColors.whiteColor),
+                label: Text('Take a Photo',
+                    style: TextStyle(color: AppColors.whiteColor)),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: AppColors.primaryLight,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25)),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/ui/home_screen/pages/diagnosisPage.dart
+++ lib/ui/home_screen/pages/diagnosisPage.dart
@@ -1,30 +1,18 @@
 import 'package:event_planning/utils/app_colors.dart';
+import 'package:event_planning/widget/picked_img_bloc_selector.dart';
 import 'package:flutter/material.dart';
-import 'dart:math';
-import 'dart:io';
 import 'diagnosis_record.dart';
+
 List<DiagnosisRecord> diagnosisHistory = [];
+
 class DiagnosisPage extends StatefulWidget {
-  final File image;
-  final String result;
-  final DateTime data;
-  const DiagnosisPage({super.key,required this.image ,
-  required this.result ,
-  required this.data});
+  const DiagnosisPage({super.key});
   @override
   State<DiagnosisPage> createState() => _DiagnosisPageState();
 }
 
 class _DiagnosisPageState extends State<DiagnosisPage> {
   bool _isLoading = true;
-  String? _diagnosisResult;
-  List<String> fakeResults = [
-    "Healthy skin detected",
-    "Mild irritation detected",
-    "Possible eczema detected",
-    "Suspicious skin condition detected",
-    "Dry skin identified"
-  ];
 
   @override
   void initState() {
@@ -38,18 +26,8 @@ class _DiagnosisPageState extends State<DiagnosisPage> {
 
     // Pick a random fake result
     setState(() {
-      _diagnosisResult = fakeResults[Random().nextInt(fakeResults.length)];
       _isLoading = false;
     });
-    if (_diagnosisResult != null) {
-      diagnosisHistory.add(
-        DiagnosisRecord(
-          image: widget.image,
-          result: _diagnosisResult!,
-          date: DateTime.now(),
-        ),
-      );
-    }
   }
 
   @override
@@ -59,66 +37,48 @@ class _DiagnosisPageState extends State<DiagnosisPage> {
       body: Center(
         child: _isLoading
             ? Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircularProgressIndicator(color: AppColors.primaryLight),
-            SizedBox(height: 20),
-            Text(
-              "Analyzing... Please wait",
-              style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: AppColors.primaryLight),
-            )
-          ],
-        )
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircularProgressIndicator(color: AppColors.primaryLight),
+                  SizedBox(height: 20),
+                  Text(
+                    "Analyzing... Please wait",
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.primaryLight),
+                  )
+                ],
+              )
             : SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.file(widget.image, height: 200),
-              SizedBox(height: 20),
-              Icon(
-                _diagnosisResult == "Healthy skin detected"
-                    ? Icons.check_circle_outline
-                    : Icons.warning_amber_rounded,
-                size: 120,
-                color: _diagnosisResult == "Healthy skin detected"
-                    ? AppColors.greenColor
-                    : AppColors.redColor,
-              ),
-              SizedBox(height: 20),
-              Text(
-                _diagnosisResult ?? "Error retrieving result",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 20),
-              Text(
-                _diagnosisResult == "Healthy skin detected"
-                    ? "Keep up your healthy skincare routine!"
-                    : "Consider consulting a dermatologist for further analysis.",
-                style:
-                TextStyle(fontSize: 16, color: AppColors.blackColor),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 30),
-              ElevatedButton.icon(
-                onPressed: () => Navigator.pop(context),
-                icon: Icon(Icons.home, color: AppColors.whiteColor),
-                label: Text("Back to Home",
-                    style: TextStyle(color: AppColors.whiteColor)),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: AppColors.primaryLight,
-                  padding:
-                  EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25),
-                  ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 20,
+                  children: [
+                    const PickedImgBlocSelector(),
+                    SizedBox(height: 20),
+                    Icon(
+                      Icons.warning_amber_rounded,
+                      size: 120,
+                      color: AppColors.redColor,
+                    ),
+                    ElevatedButton.icon(
+                      onPressed: () => Navigator.pop(context),
+                      icon: Icon(Icons.home, color: AppColors.whiteColor),
+                      label: Text("Back to Home",
+                          style: TextStyle(color: AppColors.whiteColor)),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColors.primaryLight,
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
       ),
     );
   }
