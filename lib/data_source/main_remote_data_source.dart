f28da4c4b14c466a59d9a105789a4990dac0b9be
DESKTOP-8H0HA4B\aboghaly <ahmedghaly0767@gmail.com>
2025-06-29 18:58:42 +0300

    Finished fetchRandomTip business logic

--- lib/data_source/main_remote_data_source.dart
+++ lib/data_source/main_remote_data_source.dart
@@ -2,6 +2,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../core/utils/app_utils.dart';
 import '../models/predict_response.dart';
+import '../models/tip.dart';
 
 class MainRemoteDataSource {
   final SupabaseClient _supabaseClient;
@@ -23,5 +24,13 @@ class MainRemoteDataSource {
         predictionsJson.map((e) => PredictResponse.fromJson(e)).toList();
     predictions.shuffle();
     return predictions.first;
+  }
+
+  Future<Tip> fetchRandomTip() async {
+    final remoteJson = await _fetchRemoteData();
+    final tipsJson = remoteJson['tips'] as List<dynamic>;
+    final tips = tipsJson.map((t) => Tip.fromJson(t)).toList();
+    tips.shuffle();
+    return tips.first;
   }
 }
--- lib/repositories/main_repo.dart
+++ lib/repositories/main_repo.dart
@@ -2,6 +2,7 @@ import '../core/supabase/supabase_request_result.dart';
 import '../core/utils/functions/execute_and_handle_errors.dart';
 import '../data_source/main_remote_data_source.dart';
 import '../models/predict_response.dart';
+import '../models/tip.dart';
 
 class MainRepo {
   final MainRemoteDataSource _mainRemoteDataSource;
@@ -11,6 +12,12 @@ class MainRepo {
   Future<SupabaseRequestResult<PredictResponse>> fetchRandomPrediction() {
     return executeAndHandleErrors<PredictResponse>(
       () async => await _mainRemoteDataSource.fetchRandomPrediction(),
+    );
+  }
+
+  Future<SupabaseRequestResult<Tip>> fetchRandomTip() {
+    return executeAndHandleErrors<Tip>(
+      () async => await _mainRemoteDataSource.fetchRandomTip(),
     );
   }
 }
