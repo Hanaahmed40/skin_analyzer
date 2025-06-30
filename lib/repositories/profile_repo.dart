45ae64afcf4e6851015ca42e6fa8ea29ec9943f2
DESKTOP-8H0HA4B\aboghaly <ahmedghaly0767@gmail.com>
2025-06-30 17:40:52 +0300

    Finished ProfileRepo

---
+++ lib/repositories/profile_repo.dart
@@ -0,0 +1,33 @@
+import 'package:event_planning/data_source/profile_remote_data_source.dart';
+import 'package:event_planning/models/auth_params.dart';
+import 'package:event_planning/models/user_model.dart';
+import 'package:image_picker/image_picker.dart';
+
+import '../core/supabase/supabase_request_result.dart';
+import '../core/utils/functions/execute_and_handle_errors.dart';
+
+class ProfileRepo {
+  final ProfileRemoteDataSource _remoteDataSource;
+
+  ProfileRepo(this._remoteDataSource);
+
+  Future<SupabaseRequestResult<UserModel>> updateProfile(
+    AuthParams params,
+  ) {
+    return executeAndHandleErrors<UserModel>(
+      () async => await _remoteDataSource.updateUser(params),
+    );
+  }
+
+  Future<SupabaseRequestResult<void>> logOut() {
+    return executeAndHandleErrors<void>(
+      () async => await _remoteDataSource.logOut(),
+    );
+  }
+
+  Future<SupabaseRequestResult<String>> updateProfileImg(XFile? imgFile) {
+    return executeAndHandleErrors<String>(
+      () async => await _remoteDataSource.updateProfileImg(imgFile),
+    );
+  }
+}
